5f4c5b08cdfc08686ab5b9cb72bc4e10dc89a8eb
Jakob Schlyter <jakob@kirei.se>
2025-02-27 17:28:05 +0100

    Fetch integration data from HACS

--- Makefile
+++ Makefile
@@ -1,10 +1,13 @@
 all:
 	poetry run python custom_integrations_analytics.py
 
-fetch: custom_integrations.json
+fetch: custom_integrations.json hacs_data.json
 
 custom_integrations.json:
 	curl -o $@ https://analytics.home-assistant.io/custom_integrations.json
+
+hacs_data.json:
+	curl -o $@ https://data-v2.hacs.xyz/integration/data.json
 
 clean:
 	rm -f *.json *.html
--- custom_integrations_analytics.py
+++ custom_integrations_analytics.py
@@ -2,9 +2,11 @@
 
 import argparse
 import json
+from dataclasses import dataclass
 from datetime import datetime, timezone
 from pathlib import Path
 from typing import Any
+from urllib.parse import urljoin
 
 import httpx
 import jinja2
@@ -21,6 +23,20 @@ TITLE = "Home Assistant Custom Integrations"
 
 REPOSITORY_NAME = "jschlyter/custom-integrations-analytics"
 REPOSITORY_URL = "https://github.com/jschlyter/custom-integrations-analytics"
+
+HACS_DATA_URL = "https://data-v2.hacs.xyz/integration/data.json"
+HACS_DATA_FILE = "hacs_data.json"
+
+
+@dataclass(frozen=True)
+class HacsIntegration:
+    domain: str
+    manifest_name: str
+    full_name: str
+
+    @property
+    def href(self) -> str:
+        return urljoin("https://github.com", self.full_name)
 
 
 def read_dataset(filename: Path, url: str) -> dict[str, Any]:
@@ -72,17 +88,39 @@ def main() -> None:
     args = parser.parse_args()
 
     dataset = read_dataset(filename=Path(args.input), url=args.url)
-    usage = [(name, data["total"]) for name, data in dataset.items()]
+
+    # Read integration data from HACS
+    hacs: dict[str, HacsIntegration] = {}
+    hacs_dataset = read_dataset(filename=Path(HACS_DATA_FILE), url=HACS_DATA_URL)
+    for hacs_data in hacs_dataset.values():
+        i = HacsIntegration(
+            domain=hacs_data["domain"],
+            manifest_name=hacs_data["manifest_name"],
+            full_name=hacs_data["full_name"],
+        )
+        hacs[i.domain] = i
+
+    usage = []
+    for integration, data in dataset.items():
+        if hacs_data := hacs.get(integration):
+            name = f'<a href="{hacs_data.href}">{integration}</a>'
+            description = hacs_data.manifest_name
+        else:
+            name = integration
+            description = ""
+        usage.append([name, description, data["total"]])
 
     df = pd.DataFrame(
-        sorted(usage, key=lambda v: v[1], reverse=True),
-        columns=["Integration", "Count"],
+        sorted(usage, key=lambda v: v[2], reverse=True),
+        columns=["Integration", "Description", "Count"],
     )
 
     env = jinja2.Environment(
         loader=jinja2.FileSystemLoader("."), autoescape=jinja2.select_autoescape()
     )
+
     template = env.get_template(str(args.template))
+
     with open(args.output, "w") as fp:
         table_html = df.style.pipe(make_pretty).to_html()
 
